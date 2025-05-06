bdb36885a3ca77c0cdb9ebf161129bb62797bcfa
Danny Hewitt <dannyhewitt@wifi-eduroam-default-3e-97-9a-0b-a6-3b.wlan.uclv.net>
2025-05-06 13:43:58 +0200

    Teleport System Runnning

--- VRShopping/Assets/Scripts/TeleportController.cs
+++ VRShopping/Assets/Scripts/TeleportController.cs
@@ -1,18 +1,101 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion.Teleportation;
 
 public class TeleportController : MonoBehaviour
 {
-    public GameObject[] teleportAnchors;
+    [Header("Teleport Anchors")]
+    [Tooltip("List of Teleportation Anchors to cycle through")]
+    public TeleportationAnchor[] teleportationAnchors;
 
-    void Start()
+    [Tooltip("Teleportation Provider")]
+    public TeleportationProvider teleportationProvider;
+
+    [Tooltip("Input Action Reference for triggering teleport")]
+    public InputActionReference teleportAction;
+
+    private int currentAnchorIndex = 0;
+
+    private void Start()
     {
-        //Spwan player on anchor 1
+        //Teleport to the first anchor on Start
+        if (teleportationAnchors.Length > 0 && teleportationProvider != null)
+        {
+            TeleportationAnchor anchor = teleportationAnchors[0];
+            if (anchor != null && anchor.teleportAnchorTransform != null)
+            {
+                TeleportRequest initialRequest = new TeleportRequest
+                {
+                    destinationPosition = anchor.teleportAnchorTransform.position,
+                    destinationRotation = anchor.teleportAnchorTransform.rotation,
+                    matchOrientation = MatchOrientation.TargetUpAndForward
+                };
+
+                teleportationProvider.QueueTeleportRequest(initialRequest);
+            }
+        }
     }
 
-    public void teleportToNextAnchor()
+    private void OnEnable()
+    {        
+        if (teleportAction != null)
+        {
+            teleportAction.action.performed += OnTeleport;
+        }
+    }
+
+    private void OnDisable()
     {
-        
+        if (teleportAction != null)
+        {
+            teleportAction.action.performed -= OnTeleport;
+        }
+    }
+
+    private void OnTeleport(InputAction.CallbackContext context)
+    {
+        currentAnchorIndex++;
+        try
+        {
+            TryTeleportToAnchor(currentAnchorIndex);
+        }
+        catch
+        {
+            Debug.Log("Reached end of teleport anchors. Looping back to start.");
+            currentAnchorIndex = 0;
+            TryTeleportToAnchor(currentAnchorIndex);
+        }
+    }
+
+    private void TryTeleportToAnchor(int index)
+    {
+        if (teleportationAnchors == null || teleportationAnchors.Length == 0)
+        {
+            throw new System.Exception("Teleport anchor list is empty.");
+        }
+
+        if (index < 0 || index >= teleportationAnchors.Length)
+        {
+            throw new System.IndexOutOfRangeException("Teleport anchor index out of range.");
+        }
+
+        var anchor = teleportationAnchors[index];
+        if (anchor == null || anchor.teleportAnchorTransform == null)
+        {
+            throw new System.Exception($"Teleport anchor at index '{index}' is null or missing its transform.");
+        }
+
+        var request = new TeleportRequest
+        {
+            destinationPosition = anchor.teleportAnchorTransform.position,
+            destinationRotation = anchor.teleportAnchorTransform.rotation,
+            matchOrientation = MatchOrientation.TargetUpAndForward
+        };
+
+        teleportationProvider.QueueTeleportRequest(request);
+        Debug.Log($"Teleported to anchor '{index}'");
     }
 }
