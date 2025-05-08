4a921385e600590db3488356b0425baf1649d4da
Danny Hewitt <dannyhewitt@wifi-eduroam-default-3e-97-9a-0b-a6-3b.wlan.uclv.net>
2025-05-08 13:15:18 +0200

    Item update

---
+++ VRShopping/Assets/Scripts/ItemData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ItemData : MonoBehaviour
+{
+    public string itemName;
+    [TextArea]
+    public string description;
+    public string[] hints = new string[2];
+}
--- VRShopping/Assets/Scripts/ShoppingList.cs
+++ VRShopping/Assets/Scripts/ShoppingList.cs
@@ -1,16 +1,53 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ShoppingList : MonoBehaviour
 {
-    public GameObject[] listItems;
+    public TextMeshProUGUI listDisplay;
 
-    private String[][] itemHints;
+    private GameObject[] taggedItems;
+    private bool hintsShown = false;
 
-    public void hint()
+    void Start()
     {
-        Debug.Log("Give item hint");
+        taggedItems = GameObject.FindGameObjectsWithTag("Item");
+        DisplayItems(showHints: false);
+    }
+
+    public void ShowHints()
+    {
+        if (!hintsShown)
+        {
+            hintsShown = true;
+            DisplayItems(showHints: true);
+        }
+    }
+
+    private void DisplayItems(bool showHints)
+    {
+        listDisplay.text = "";
+
+        foreach (GameObject obj in taggedItems)
+        {
+            ItemData data = obj.GetComponent<ItemData>();
+            if (data != null)
+            {
+                listDisplay.text += $"<b>{data.itemName}</b>\n";
+                listDisplay.text += $"{data.description}\n";
+
+                if (showHints)
+                {
+                    foreach (string hint in data.hints)
+                    {
+                        listDisplay.text += $"- {hint}\n";
+                    }
+                }
+
+                listDisplay.text += "\n";
+            }
+        }
     }
 }
